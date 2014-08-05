93f2107d4259522b5fa2507e42143009d83cbaba
Patrik Gothe <patrik.got@gmail.com>
2014-08-05 11:39:18 +0200

    implementing basic related artist list dump

--- app/client/index.js
+++ app/client/index.js
@@ -2,6 +2,12 @@ Router.configure({
 	layoutTemplate: 'main'
 });
 
+var artists = {
+	'jonsi': {id:"3khg8RDB6nMuw34w1IHS6Y"},
+	'The Black Keys': {id:"7mnBLXK823vNxN3UWB7Gfz"}
+}
+
+
 Router.map(function() {
 	this.route('home', {
 		path: '/',
@@ -11,15 +17,37 @@ Router.map(function() {
 
 
 if (Meteor.isClient) {
+	Meteor.startup(function () {
+	  	$.ajax({
+	  	    url: 'https://api.spotify.com/v1/artists/'+artists["jonsi"].id,
+	  	    success: function (response) {
+
+	  	        Session.set("dump", response);
+	  	    }
+	  	});
+
+
+	  	$.ajax({
+	  	    url: 'https://api.spotify.com/v1/artists/'+artists["jonsi"].id+'/related-artists',
+	  	    success: function (response) {
+	  	        Session.set("related", response);
+	  	    }
+	  	});
+
+
+	});
+
   Template.index.dump = function () {
-  	$.ajax({
-  	    url: 'https://api.spotify.com/v1/albums/1fgtiyeUDIoUayYbwroqVH',
-  	    success: function (response) {
-  	        // return JSON.prresponse;
-  	    }
-  	});
+  	return Session.get("dump");
   };
+
+  Template.index.related = function () {
+  	return Session.get("related");
+  };
+
 }
+
+
 
 if (Meteor.isServer) {
 }
