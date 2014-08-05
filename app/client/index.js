523a9c71a2e95e1cc12a8b96a9ae582f86f1e96a
Patrik Gothe <patrik.got@gmail.com>
2014-08-05 12:35:59 +0200

    demo of related artists navigation

--- app/client/index.js
+++ app/client/index.js
@@ -16,34 +16,49 @@ Router.map(function() {
 });
 
 
+
 if (Meteor.isClient) {
 	Meteor.startup(function () {
+	  	
+		fetchActiveArtist(artists["jonsi"].id);
+		fetchRelatedArtists(artists["jonsi"].id);
+	});
+
+	function fetchActiveArtist(artistId) {
+		$.ajax({
+		    url: 'https://api.spotify.com/v1/artists/'+artistId,
+		    success: function (response) {
+
+		        Session.set("artist", response);
+		    }
+		});
+	}
+
+	function fetchRelatedArtists(artistId) {
 	  	$.ajax({
-	  	    url: 'https://api.spotify.com/v1/artists/'+artists["jonsi"].id,
-	  	    success: function (response) {
-
-	  	        Session.set("dump", response);
-	  	    }
-	  	});
-
-
-	  	$.ajax({
-	  	    url: 'https://api.spotify.com/v1/artists/'+artists["jonsi"].id+'/related-artists',
+	  	    url: 'https://api.spotify.com/v1/artists/'+artistId+'/related-artists',
 	  	    success: function (response) {
 	  	        Session.set("related", response);
 	  	    }
 	  	});
+	}
 
+	
 
-	});
-
-  Template.index.dump = function () {
-  	return Session.get("dump");
+  Template.index.artist = function () {
+  	return Session.get("artist");
   };
 
   Template.index.related = function () {
   	return Session.get("related");
   };
+
+  Template.index.events({
+  	'click ul.related-artists li': function () {
+  		fetchActiveArtist(this.id);
+  		fetchRelatedArtists(this.id);
+  	}
+  });
 
 }
 
