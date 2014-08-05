4f0d17aef49197ed11b0a3bebd735a322f34bdb3
Oscar Nilsson <mail@oscr.se>
2014-08-05 19:50:30 +0200

    insert to db ok

--- app/client/highscore.js
+++ app/client/highscore.js
@@ -1,7 +1,3 @@
-
-Highscores = new Meteor.Collection('highscores');
-
-
 if (Meteor.isClient) {
 	Meteor.startup(function () {
 	
@@ -10,30 +6,42 @@ if (Meteor.isClient) {
 
 
 
-
 		Session.set("finalScore", 1000);
+		Session.set("tag", 'username');
+		Session.set("startingArtistId", '12345');
+		Session.set("endingArtistId", '54321');
 
 
 		Template.toplist.finalScore = function () {
 			return Session.get("finalScore");
 		};
-		
+
 		Template.toplist.events = {
+
+			
+
+
 		    'submit form.yo': function (e) {
-		    	e.preventDefault();
-			    var yoName = $("#yoText").val();
 
-			    console.log(yoName)
+		    var score = Session.get("finalScore");
+			var yoName = $("#yoText").val();
+			var id1 = Session.get("startingArtistId");
+			var id2 = Session.get("endingArtistId");
 
 
-			    $.ajax({
-			    	type: "POST",
-				    url: 'http://api.justyo.co/yo/',
-				    data: 'api_token=797471a2-1e3f-0e8e-ee3d-342bf3b86f43&username=' + yoName,
-				    success: function (response) {
-				    	//do nothing
-		   			}
-				});
+
+			Highscores.insert({score: score, name: yoName, id1: id1, id2: id2 });
+
+	    	e.preventDefault();
+
+		    $.ajax({
+		    	type: "POST",
+			    url: 'http://api.justyo.co/yo/',
+			    data: 'api_token=797471a2-1e3f-0e8e-ee3d-342bf3b86f43&username=' + yoName,
+			    success: function (response) {
+			    	//do nothing
+	   			}
+			});
 		    }
 		};
 
