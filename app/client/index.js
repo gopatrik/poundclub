56eddff6f1a6abe1ebab689616487a7d1a3cc9ed
Patrik Gothe <patrik.got@gmail.com>
2014-08-05 11:14:57 +0200

    adding spotify ajax request sample

--- app/client/index.js
+++ app/client/index.js
@@ -1,9 +1,25 @@
-if (Meteor.isClient) {
+Router.configure({
+	layoutTemplate: 'main'
+});
 
+Router.map(function() {
+	this.route('home', {
+		path: '/',
+		template:'index'
+	});
+});
+
+
+if (Meteor.isClient) {
+  Template.index.rendered = function () {
+  	$.ajax({
+  	    url: 'https://api.spotify.com/v1/albums/1fgtiyeUDIoUayYbwroqVH',
+  	    success: function (response) {
+  	        console.log(response);
+  	    }
+  	});
+  };
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-
-  });
 }
