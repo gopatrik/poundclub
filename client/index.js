70e0e55b535542adc18b8fac554473416577f631
Patrik Gothe <patrik.got@gmail.com>
2014-08-06 14:06:21 +0200

    remove some things, ding ding dings

--- client/index.js
+++ client/index.js
@@ -241,10 +241,6 @@ if (Meteor.isClient) {
 		document.onkeydown = undefined;
 	};
 
-	Template.missions.name = function () {
-		return "Searching for sugar man";
-	}
-
 	Template.missions.current = function () {
 		var goalArtist = Session.get("goalArtist");
 		var startArtist = Session.get("startArtist");
