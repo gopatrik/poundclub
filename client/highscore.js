5cc446336366e62326f1fa13444e4d44c22c0aba
Oscar Nilsson <mail@oscr.se>
2014-08-09 16:36:07 +0200

    Large tweet

--- client/highscore.js
+++ client/highscore.js
@@ -84,8 +84,9 @@ if (Meteor.isClient) {
   			js = d.createElement(s); js.id = id;
   			js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
   			fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-  			!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+			}(document, 'script', 'facebook-jssdk'));			
+
+			!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 	};
 	
 	Template.index.events = {
