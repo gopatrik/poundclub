7f06b7bfef4031aaeaefafb79d08a78eb5ab5359
Oscar Nilsson <mail@oscr.se>
2014-08-19 00:51:07 +0200

    new db setup artists

--- server/dbsetup.js
+++ server/dbsetup.js
@@ -5,3 +5,11 @@
 // SetupArtists.insert({start: "1cHFz6lrt7KAsBV8j2Ny1g", goal: "776Uo845nYHJpNaStv1Ds4", popularity: 0 });
 // SetupArtists.insert({start: "2wOqMjp9TyABvtHdOSOTUS", goal: "3MKCzCnpzw3TjUYs2v7vDA", popularity: 0 });	
 // }
+// SetupArtists.insert({start: "1dfeR4HaWDbWqFHLkxsg1d", goal: "3eskO5m0H4yiF64vRySBjr", popularity: 0 });	
+
+// SetupArtists.insert({start: "1GLtl8uqKmnyCWxHmw9tL4", goal: "2DaxqgrOhkeH0fpeiQq2f4", popularity: 0 });	
+// SetupArtists.insert({start: "0L8ExT028jH3ddEcZwqJJ5", goal: "2ye2Wgw4gimLv2eAKyk1NB", popularity: 0 });	
+// SetupArtists.insert({start: "1G9G7WwrXka3Z1r7aIDjI7", goal: "4tujQJicOnuZRLiBFdp3Ou", popularity: 0 });	
+// SetupArtists.insert({start: "6sFIWsNpZYqfjUpaCgueju", goal: "1l7ZsJRRS8wlW3WfJfPfNS", popularity: 0 });	
+// SetupArtists.insert({start: "1Cs0zKBU1kc0i8ypK3B9ai", goal: "1XpDYCrUJnvCo9Ez6yeMWh", popularity: 0 });	
+// SetupArtists.insert({start: "4pejUc4iciQfgdX6OKulQn", goal: "2cCUtGK9sDU2EoElnk0GNB", popularity: 0 });	
